a98afa45e23fc7733468b739ed29e50d5c3b6dca
shashankm <shashank.shashi2@gmail.com>
2015-11-17 10:25:36 +0530

    Edited sign up page

--- app/src/main/java/com/cybrilla/shashank/liborg/SignUp.java
+++ app/src/main/java/com/cybrilla/shashank/liborg/SignUp.java
@@ -6,19 +6,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 public class SignUp extends AppCompatActivity {
+    private TextView nameIcon, checkIcon, keyIconSignUp, mailIconSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        TextView mailIconSignUp = (TextView) findViewById(R.id.mailIconSignUp);
-        TextView keyIconSignUp = (TextView) findViewById(R.id.keyIconSignUp);
-        TextView checkIcon = (TextView) findViewById(R.id.checkIcon);
+        mailIconSignUp = (TextView) findViewById(R.id.mailIconSignUp);
+        keyIconSignUp = (TextView) findViewById(R.id.keyIconSignUp);
+        checkIcon = (TextView) findViewById(R.id.checkIcon);
+        nameIcon = (TextView) findViewById(R.id.nameIcon);
 
         Typeface font = Typeface.createFromAsset(getAssets(), "fontawesome-webfont.ttf");
         mailIconSignUp.setTypeface(font);
         keyIconSignUp.setTypeface(font);
         checkIcon.setTypeface(font);
+        nameIcon.setTypeface(font);
     }
 }
