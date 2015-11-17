883e058b9cbce5f9f76451a6457501cce6682193
shashankm <shashank.shashi2@gmail.com>
2015-11-17 19:37:35 +0530

    Barcode scanner added

--- app/src/main/java/com/cybrilla/shashank/liborg/LibraryActivity.java
+++ app/src/main/java/com/cybrilla/shashank/liborg/LibraryActivity.java
@@ -1,5 +1,6 @@
 package com.cybrilla.shashank.liborg;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -8,7 +9,10 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+
+import com.google.zxing.integration.android.IntentIntegrator;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -70,5 +74,25 @@ public class LibraryActivity extends AppCompatActivity {
         public CharSequence getPageTitle(int position) {
             return mFragmentTitleList.get(position);
         }
+    }
+
+    public void barCode(View v){
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ONE_D_CODE_TYPES);
+        integrator.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            String _code = data.getStringExtra("SCAN_RESULT");
+            Log.e("Library Activity", "Code: "+_code);
+            // do whatever you want
+
+        }
+
     }
 }
