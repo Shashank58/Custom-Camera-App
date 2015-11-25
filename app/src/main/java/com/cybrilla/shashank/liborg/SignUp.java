fd13d8377696a5f270639885d0455e20aeb66563
shashankm <shashank.shashi2@gmail.com>
2015-11-25 15:53:57 +0530

    Real sign up working

--- app/src/main/java/com/cybrilla/shashank/liborg/SignUp.java
+++ app/src/main/java/com/cybrilla/shashank/liborg/SignUp.java
@@ -1,14 +1,36 @@
 package com.cybrilla.shashank.liborg;
 
-import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignUp extends AppCompatActivity {
     private TextView fnameIcon, checkIcon, keyIconSignUp, mailIconSignUp,lnameIcon;
+    private String mFname, mLname, mEmail, mPassword;
+    private EditText fname, lname, email, password;
+    public static final String KEY_FNAME = "firstname";
+    public static final String KEY_LNAME = "lastname";
+    public static final String KEY_EMAIL = "email";
+    public static final String KEY_PASSWORD = "password";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +43,11 @@ public class SignUp extends AppCompatActivity {
         fnameIcon = (TextView) findViewById(R.id.fnameIcon);
         lnameIcon = (TextView) findViewById(R.id.lnameIcon);
 
+        fname = (EditText) findViewById(R.id.fname);
+        lname = (EditText) findViewById(R.id.lname);
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+
         Typeface font = Typeface.createFromAsset(getAssets(), "fontawesome-webfont.ttf");
         mailIconSignUp.setTypeface(font);
         keyIconSignUp.setTypeface(font);
@@ -30,7 +57,57 @@ public class SignUp extends AppCompatActivity {
     }
 
     public void loginScreen(View v){
-        Intent intent = new Intent(this, LogInActivity.class);
-        startActivity(intent);
+        registerUser();
+    }
+
+    public void registerUser(){
+        mFname = fname.getText().toString();
+        mLname = lname.getText().toString();
+        mEmail = email.getText().toString();
+        mPassword = password.getText().toString();
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        String REGISTER_URL = "https://liborgs-1139.appspot.com/users/register?" +
+                "";
+
+        StringRequest jsonObjectRequest = new StringRequest(Request.Method.POST, REGISTER_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            Log.e("Sign Up", "Response is: "+response);
+                            JSONObject jObj = new JSONObject(response);
+                            Boolean status = jObj.getBoolean("status");
+                            Log.e("SIgn Up", "Status: " + status);
+                            if(status){
+                                Toast.makeText(getApplicationContext(), "Register successful" +
+                                        "Please check your mail", Toast.LENGTH_LONG).show();
+                            }
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() {
+                // Posting params to register url
+                Map<String, String> params = new HashMap<>();
+                params.put(KEY_FNAME, mFname);
+                params.put(KEY_LNAME, mLname);
+                params.put(KEY_EMAIL, mEmail);
+                params.put(KEY_PASSWORD, mPassword);
+                Log.e("Sign Up", "Params: "+params.toString());
+                return params;
+            }
+        };
+
+        queue.add(jsonObjectRequest);
     }
 }
